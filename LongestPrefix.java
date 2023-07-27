9e076aecb6bc63b927f9323d4f95ee0cfbb1e520
purnima-gosain <gosainpurnima916@gmail.com>
2023-07-27 15:36:23 +0545

    added commonPrefix method

--- LongestPrefix.java
+++ LongestPrefix.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class LongestPrefix {
-    
+    public String commonPrefix(String[] arr){
+        int n = arr.length;
+        String result = arr[0];
+
+        for(int i =1; i<n; i++){
+            while(arr[i].indexOf(result) != 0){
+                result= result.substring(0, result.length()-1);
+
+                if(result.isEmpty()){
+                    return "-1";
+                }
+            }
+        }
+        return result;
+    }
 }
