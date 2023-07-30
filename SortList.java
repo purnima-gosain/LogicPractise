4e093feea699496fa9c41b21284ad134d87ec546
purnima-gosain <gosainpurnima916@gmail.com>
2023-07-30 18:12:40 +0545

    added new function

--- SortList.java
+++ SortList.java
@@ -1,3 +1,41 @@
+import java.util.*;
+
 public class SortList {
+
+    int temp = 0;
+    public void newMethod(){
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+    list1.add(0);
+    list1.add(1);
+    list1.add(2);
+
+    ArrayList<Integer> list2 = new ArrayList<Integer>();
+    list2.add(0);
+    list2.add(3);
+    list2.add(4);
+
+    list1.addAll(list2);
+    // ArrayList<Integer> mergeList = new ArrayList<Integer>(list1);
+    // mergeList.addAll( list2);
+    System.out.println("Old list"+list1);
+
+    for(int i = 0;i < list1.size();i++){
+        System.out.println("hhhhhh");
+        for(int j =i+1;j< list1.size(); j++){
+            System.out.println("llllll");
+            if(list1.get(i) > list1.get(j)){
+                temp = list1.get(i);
+                list1.add(i,list1.get(j));
+                list1.add(j,temp);
+            }
+        }
+    }
     
+    }
+    
+   public static void main(String[] args){
+    SortList abc = new SortList();
+    abc.newMethod();
+   }
+
 }
